dc9e11787170b385917cccba66bd6d7444b297fa
Willy DESPLAS <willy.desplas@orange.com>
2019-03-13 13:04:44 +0000

    Set max connection

--- examples/docker/etc/keeper-entrypoint.sh
+++ examples/docker/etc/keeper-entrypoint.sh
@@ -3,7 +3,7 @@
 # The command-line options specified in this script are expected NOT to change.
 # To override the Keeper's entrypoint, you will have to provide the appropriate values for all the options. In particular, the user assigned to `--pg-su-username` must exist in the container, and `--pg-listen-address` must be unique, discoverable and accessible by other containers and replicas of Keeper.
 
-sed -i.save s/100/${MAX_CONNECTIONS}/g ${STKEEPER_DATA_DIR}/postgres/postgresql.conf
+sed -i.save s/100/300/g ${STKEEPER_DATA_DIR}/postgres/postgresql.conf
 
 stolon-keeper \
   --pg-su-username=postgres \
